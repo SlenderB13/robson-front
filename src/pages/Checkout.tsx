793e9a274da7fe11ebbc2ce406cd386320ee1b44
Lucas Silva <lguilherme8@gmail.com>
2023-06-15 12:35:59 -0300

    edit: usando state de loading em tela de vendas

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Box, Button, Grid, Sheet, Table, Typography } from "@mui/joy"
+import { Autocomplete, Box, Button, CircularProgress, Grid, Sheet, Table, Typography } from "@mui/joy"
 import { Product } from "../interfaces/Product"
 import { useState } from "react"
 import { useFetch } from "../hooks/useFetch";
@@ -7,7 +7,7 @@ import { useDelete } from "../hooks/useDelete";
 export const Checkout = () => {
     const [selectedProducts, setSelectedProducts] = useState<Product[] | null>(null);
 
-    const products = useFetch<Product[]>('products')
+    const {data: products, isLoading} = useFetch<Product[]>('products')
 
     const handleCheckout = () => {
         selectedProducts?.map((product) => {
@@ -19,64 +19,76 @@ export const Checkout = () => {
         <Box sx={{
             width: '50rem'
         }}>
-            <Box component='section' sx={{
-                display: 'flex',
-                flexDirection: 'column',
-            }}>
-                <Typography level='h3' mb={2}>Realize sua venda</Typography>
-                <Typography>Selecione o(s) produto(s):</Typography>
-                <Autocomplete
-                    multiple
-                    placeholder="items em estoque"
-                    options={products ? products : []}
-                    getOptionLabel={option => option.name}
-                    onChange={(e, items: Product[]) => setSelectedProducts(items)}
-                    sx={{
+            {!isLoading ? (
+                <Box component='section' sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}>
+                    <Typography level='h3' mb={2}>Realize sua venda</Typography>
+                    <Typography>Selecione o(s) produto(s):</Typography>
+                    <Autocomplete
+                        multiple
+                        placeholder="items em estoque"
+                        options={products ? products : []}
+                        getOptionLabel={option => option.name}
+                        onChange={(e, items: Product[]) => setSelectedProducts(items)}
+                        sx={{
+                            marginBottom: '2rem'
+                        }}
+                    />
+                    <Typography>Confira os produtos:</Typography>
+                    <Sheet sx={{
                         marginBottom: '2rem'
+                    }}>
+                        <Table
+                        borderAxis="x"
+                        color="neutral"
+                        size="lg"
+                        stickyFooter
+                        stickyHeader
+                        stripe="even"
+                        variant="plain"
+                        >
+                            <thead>
+                                <tr>
+                                    <th>Produto</th>
+                                    <th>Valor</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {selectedProducts?.map((product, index) => (
+                                <tr key={index}>
+                                    <td>{product.name}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>TOTAL:</th>
+                                    <td>
+                                        {selectedProducts?.reduce((actualPrice=0, product) => actualPrice + product.price, 0)}
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </Table>
+                    </Sheet>
+                    <Button 
+                        disabled={!selectedProducts ? true : false}
+                        onClick={handleCheckout}
+                    >Finalizar venda</Button>
+                </Box>
+
+            ) : (
+                <CircularProgress
+                    size='lg' 
+                    sx={{
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%'
                     }}
                 />
-                <Typography>Confira os produtos:</Typography>
-                <Sheet sx={{
-                    marginBottom: '2rem'
-                }}>
-                    <Table
-                    borderAxis="x"
-                    color="neutral"
-                    size="lg"
-                    stickyFooter
-                    stickyHeader
-                    stripe="even"
-                    variant="plain"
-                    >
-                        <thead>
-                            <tr>
-                                <th>Produto</th>
-                                <th>Valor</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {selectedProducts?.map((product, index) => (
-                            <tr key={index}>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>TOTAL:</th>
-                                <td>
-                                    {selectedProducts?.reduce((actualPrice=0, product) => actualPrice + product.price, 0)}
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </Table>
-                </Sheet>
-                <Button 
-                    disabled={!selectedProducts ? true : false}
-                    onClick={handleCheckout}
-                >Finalizar venda</Button>
-            </Box>
+            )}
         </Box>
     )
 }
