460a1a4fa3196e692cbfc23f72156a47f64048e1
Lucas Silva <lguilherme8@gmail.com>
2023-06-15 12:32:42 -0300

    edit: usando state de loading

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,10 @@
-import { Box, Grid, Typography } from "@mui/joy"
+import { Box, CircularProgress, Grid, Typography } from "@mui/joy"
 import { Product } from "../interfaces/Product"
 import { ProductCard } from "../components/ProductCard"
 import { useFetch } from "../hooks/useFetch"
 
 export const Products = () => {
-    const products = useFetch<Product[] | null>('products')
+    const {data: products, isLoading} = useFetch<Product[] | null>('products')
 
     return (
         <Box sx={{
@@ -13,19 +13,32 @@ export const Products = () => {
             alignItems: 'center',
             justifyContent: 'center'
         }}>
-            <Typography level='h3' mb={2}>Produtos em estoque</Typography>
-            <Grid
-                container
-                spacing={{ xs: 2, md: 3 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-                sx={{ flexGrow: 1 }}
-            >
-            {products?.map((product, index) => (
-                <Grid xs={2} sm={4} md={4} key={index}>
-                    <ProductCard brand={product.brand} name={product.name} price={product.price} amount={product.amount} />
-                </Grid>
-            ))}
-            </Grid>
+            {!isLoading ? (
+                <>
+                    <Typography level='h3' mb={2}>Produtos em estoque</Typography>
+                    <Grid
+                        container
+                        spacing={{ xs: 2, md: 3 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
+                        sx={{ flexGrow: 1 }}
+                    >
+                    {products?.map((product, index) => (
+                        <Grid xs={2} sm={4} md={4} key={index}>
+                            <ProductCard brand={product.brand} name={product.name} price={product.price} amount={product.amount} />
+                        </Grid>
+                    ))}
+                    </Grid>
+                </>
+            ) : (
+                <CircularProgress 
+                    size='lg' 
+                    sx={{
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%'
+                    }}
+                />
+            )}
         </Box>
     )
 }
