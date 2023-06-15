068607fca21d93e567e77494c0f24844569435cd
Lucas Silva <lguilherme8@gmail.com>
2023-06-14 22:45:20 -0300

    edit: novas infos no card do produto

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,16 +1,16 @@
 import { Card, CardContent, Typography } from "@mui/joy"
 import { Product } from "../interfaces/Product"
 
-export const ProductCard = ({name, price, amount}: Product) => {
+export const ProductCard = ({name, price, brand}: Product) => {
     return (
         <Card sx={{ width: 320, maxWidth: '100%', boxShadow: 'lg' }}>
             <CardContent>
-                <Typography level="body3">adicionar tipo ou marca</Typography>
+                <Typography level="body3">{brand}</Typography>
                 <Typography fontWeight="xl">{name}</Typography>
 
-                <Typography fontSize="xl" fontWeight="xl" sx={{ mt: 1 }}>{price}</Typography>
-                <Typography level="body2">
-                (<b>{amount}</b> em estoque)
+                <Typography fontSize="xl" fontWeight="xl" sx={{ mt: 1 }}>
+                    <Typography level='body2' fontWeight='lg'>R$ </Typography>
+                    {price}
                 </Typography>
             </CardContent>
         </Card>
