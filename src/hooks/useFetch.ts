527a829497b46b6be6fb08ce01139f96dd66d2c0
Lucas Silva <lguilherme8@gmail.com>
2023-06-15 12:32:26 -0300

    add: state de loading

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,13 +3,20 @@ import { useEffect, useState } from "react"
 
 export const useFetch = <T>(url: string) => {
     const [data, setData] = useState<T | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     useEffect(() => {
+        setIsLoading(true)
         axios.get(`${import.meta.env.VITE_REACT_APP_BASE_URL}/${url}`)
         .then((response: AxiosResponse) => {
             setData(response.data)
         })
+        .catch((err) => 
+        console.log(err))
+        .finally(() => {
+            setIsLoading(false)
+        })
     }, [])
 
-    return data;
+    return {data, isLoading};
 }
