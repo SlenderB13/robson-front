ac5a2f6e9ead877ceed6be4cb41b9e8146b81737
Lucas Silva <lguilherme8@gmail.com>
2023-06-14 22:46:54 -0300

    edit: usando chamada de api para exibicao de produtos

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,29 +1,14 @@
 import { Box, Grid, Typography } from "@mui/joy"
 import { Product } from "../interfaces/Product"
 import { ProductCard } from "../components/ProductCard"
+import { useFetch } from "../hooks/useFetch"
 
 export const Products = () => {
-    const products: Product[] = [
-        {
-            "name": "Z Flip 3",
-            "price": 4.999,
-            "amount": 2
-        },
-        {
-            "name": "Z Flip 4",
-            "price": 6.999,
-            "amount": 1
-        },
-        {
-            "name": "Galaxy Fold",
-            "price": 7.999,
-            "amount": 4
-        }
-    ]
+    const products = useFetch<Product[] | null>('products')
 
     return (
         <Box sx={{
-            width: '100%',
+            width: '80rem',
             height: '100%',
             alignItems: 'center',
             justifyContent: 'center'
@@ -35,9 +20,9 @@ export const Products = () => {
                 columns={{ xs: 4, sm: 8, md: 12 }}
                 sx={{ flexGrow: 1 }}
             >
-            {products.map((product, index) => (
+            {products?.map((product, index) => (
                 <Grid xs={2} sm={4} md={4} key={index}>
-                    <ProductCard name={product.name} price={product.price} amount={product.amount} />
+                    <ProductCard brand={product.brand} name={product.name} price={product.price} amount={product.amount} />
                 </Grid>
             ))}
             </Grid>
